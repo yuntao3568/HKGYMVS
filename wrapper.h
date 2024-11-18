e5aadbdc01ea698e167b02e774cf4bae0a0352ca
longz <longzezheng@126.com>
2024-11-18 17:59:22 +0800

    1

---
+++ wrapper.h
@@ -0,0 +1,7 @@
+#include "./Development/Includes/CameraParams.h"
+#include "./Development/Includes/MvCameraControl.h"
+#include "./Development/Includes/MvErrorDefine.h"
+#include "./Development/Includes/MvISPErrorDefine.h"
+#include "./Development/Includes/MvObsoleteInterfaces.h"
+#include "./Development/Includes/ObsoleteCamParams.h"
+#include "./Development/Includes/PixelType.h"
